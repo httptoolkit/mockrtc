955219465527235907447ba83831294795893226
Tim Perry <pimterry@gmail.com>
2022-03-09 19:31:00 +0100

    Extract test setup into its own reusable file & don't hide real errors
    Previously, real failures would be ignored and retried heavily until the
    timeout ran out. Now any network errors are ignored, but any other
    errors immediately trigger a failure.

--- test/integration/smoke-test.spec.ts
+++ test/integration/smoke-test.spec.ts
@@ -1,28 +1,7 @@
-import { expect } from "chai";
-
-import * as MockRTC from "../../src/main-browser";
-
-// When running continuous tests, where the admin server restarts, we have a race condition
-// between server restart & test run. To fix that, here we wait 10s for the admin server to
-// become accessible before we run any tests.
-before(async function () {
-    this.timeout(10000);
-    let shownMessage = false;
-
-    while (true) {
-        try {
-            const server = MockRTC.getRemote();
-            await server.start();
-            await server.stop();
-            break;
-        } catch (e) {
-            if (!shownMessage) {
-                console.log("Waiting for admin server to start...");
-                shownMessage = true;
-            }
-        }
-    }
-});
+import {
+    MockRTC,
+    expect
+} from '../test-setup';
 
 describe("MockRTC", function () {
 
