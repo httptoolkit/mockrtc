bd070fe2173423fa7858be6cf52fa7b90b827671
Tim Perry <pimterry@gmail.com>
2022-09-05 14:40:34 +0200

    Export matcher definitions in browser too

--- src/main-browser.ts
+++ src/main-browser.ts
@@ -11,6 +11,7 @@ import { MockRTCClient, MockRTCClientOptions } from "./client/mockrtc-client";
 
 // Export the required structures to remotely build and send rules to the admin API:
 export * as HandlerStepDefinitions from "./handling/handler-step-definitions";
+export * as MatcherDefinitions from "./matching/matcher-definitions";
 export { MockRTCAdminRequestBuilder } from "./client/mockrtc-admin-request-builder";
 
 export type {
